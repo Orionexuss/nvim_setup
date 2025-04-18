4e285d5a3ded87f3274307bb449fcb2b9fcfc567
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-04-18 09:44:23 -0500

    feat: add keymaps for LSP functionality

--- lua/setup_telescope.lua
+++ lua/setup_telescope.lua
@@ -22,3 +22,9 @@ require('telescope').setup({
 })
 
 require('telescope').load_extension('harpoon')
+
+-- Keymaps for lsp
+vim.keymap.set('n', 'gd', function() require('telescope.builtin').lsp_definitions() end, { noremap = true, silent = true })
+vim.keymap.set('n', 'gD', function() require('telescope.builtin').lsp_declarations() end, { noremap = true, silent = true })
+vim.keymap.set('n', 'gi', function() require('telescope.builtin').lsp_implementations() end, { noremap = true, silent = true })
+vim.keymap.set('n', 'gr', function() require('telescope.builtin').lsp_references() end, { noremap = true, silent = true })
