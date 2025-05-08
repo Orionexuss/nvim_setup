9cfe0c6e227ecce383b31a1ca9bdf07e5c30a604
Orionexuss <sebastianvargaslopez36@gmail.com>
2025-05-07 23:52:09 -0500

    feat: add new colorscheme (modus-themes.nvim)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -329,5 +329,6 @@ return require("lazy").setup({
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
-    },
+	},
+	{ "miikanissi/modus-themes.nvim" },
 })
